c7fde8066c8903385a453f3b31c5520a53d3bfc3
VLAD228222 <zxc710962@gmail.com>
2025-05-28 15:12:33 +0300

    Add files via upload

---
+++ Program№1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class Program
+{
+    static void PrintMultiplicationTable(int number)
+    {
+        if (number == 0)
+        {
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    Console.Write($"{i} * {j} = {i * j}\t");
+                }
+                Console.WriteLine();
+            }
+        }
+        else
+        {
+            for (int i = 1; i <= 9; i++)
+            {
+                Console.WriteLine($"{number} * {i} = {number * i}");
+            }
+        }
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Автор: Дейко Влад");
+
+        int number;
+        while (true)
+        {
+            Console.Write("Введіть число від 1 до 9 (або 0 для повної таблиці): ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out number) && number >= 0 && number <= 9)
+            {
+                break; 
+            }
+            else
+            {
+                Console.WriteLine(" Помилка: введено некоректне число. Спробуйте ще раз.\n");
+            }
+        }
+
+        Console.WriteLine("\n Таблиця множення:\n");
+        PrintMultiplicationTable(number);
+
+        Console.WriteLine("\nНатисніть будь-яку клавішу для завершення...");
+        Console.ReadKey();
+    }
+}
