915813c085ae7003b511a20eb3bb6f83caddcf3e
VLAD228222 <zxc710962@gmail.com>
2025-05-28 19:38:40 +0300

    Add files via upload

---
+++ Program№5.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Swap(ref double a, ref double b)
+    {
+        double temp = a;
+        a = b;
+        b = temp;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Автор: Дейко Влад");
+
+        double x, y;
+
+        while (true)
+        {
+            Console.Write("Введіть перше число: ");
+            if (double.TryParse(Console.ReadLine(), out x))
+                break;
+            else
+                Console.WriteLine("❌ Помилка: введено некоректне число. Спробуйте ще раз.");
+        }
+
+        while (true)
+        {
+            Console.Write("Введіть друге число: ");
+            if (double.TryParse(Console.ReadLine(), out y))
+                break;
+            else
+                Console.WriteLine("❌ Помилка: введено некоректне число. Спробуйте ще раз.");
+        }
+
+        Console.WriteLine($"\nДо обміну: x = {x}, y = {y}");
+
+        Swap(ref x, ref y);
+
+        Console.WriteLine($"Після обміну: x = {x}, y = {y}");
+
+        Console.WriteLine("\nНатисніть будь-яку клавішу для завершення...");
+        Console.ReadKey();
+    }
+}
