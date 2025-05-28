a7fa78f003e55bd9e4219c42216b18073699c0ab
VLAD228222 <zxc710962@gmail.com>
2025-05-28 19:31:05 +0300

    Add files via upload

---
+++ Program№4.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program
+{
+    static string TranslateSeason(string seasonUa)
+    {
+        switch (seasonUa.Trim().ToLower())
+        {
+            case "весна":
+                return "Spring";
+            case "літо":
+                return "Summer";
+            case "осінь":
+                return "Autumn";
+            case "зима":
+                return "Winter";
+            default:
+                return null; 
+        }
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Автор: Дейко Влад");
+
+        string inputSeason;
+        string translated;
+
+        while (true)
+        {
+            Console.Write("Введіть назву пори року українською мовою: ");
+            inputSeason = Console.ReadLine();
+
+            translated = TranslateSeason(inputSeason);
+
+            if (translated != null)
+            {
+                Console.WriteLine($"\nАнглійською: {translated}");
+                break;
+            }
+            else
+            {
+                Console.WriteLine(" Помилка: введено невідому пору року. Спробуйте ще раз.\n");
+            }
+        }
+
+        Console.WriteLine("\nНатисніть будь-яку клавішу для завершення...");
+        Console.ReadKey();
+    }
+}
